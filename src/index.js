90425f1fc05a8405c40b69dde2f0eb01f1159b21
Alex Cooksey <aecooksey2651@gmail.com>
2021-12-07 19:28:25 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    addToDo(e.target.new_task_description.value)
+    form.reset()
+  })
+  function addToDo(todo) {
+    let p = document.createElement('p')
+    let btn = document.createElement('button')
+    btn.textContent = 'Remove'
+    p.textContent = `${todo}   `
+    p.appendChild(btn).addEventListener('click', deleteEvent)
+    document.getElementById('tasks').appendChild(p)
+  }
+  function deleteEvent(e) {
+    e.target.parentNode.remove()
+  }
+})
+
