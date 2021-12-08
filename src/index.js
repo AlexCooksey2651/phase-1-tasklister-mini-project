95c6e0d90df3c3bbfdb3a412d918b5304958da04
Alex Cooksey <aecooksey2651@gmail.com>
2021-12-08 13:14:17 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -2,17 +2,26 @@ document.addEventListener("DOMContentLoaded", () => {
   let form = document.querySelector('form')
   form.addEventListener('submit', (e) => {
     e.preventDefault()
-    addToDo(e.target.new_task_description.value)
+    addToDo(e.target.new_task_description.value, e.target.date.value)
     form.reset()
   })
-  function addToDo(todo) {
-    let p = document.createElement('p')
-    let btn = document.createElement('button')
-    btn.textContent = 'Remove'
-    p.textContent = `${todo}   `
-    p.appendChild(btn).addEventListener('click', deleteEvent)
-    document.getElementById('tasks').appendChild(p)
+  
+  function addToDo(todo, date) {
+    let li = document.createElement('li')
+    li.textContent = `${todo}, Due: ${date}   `
+
+    let textColor = document.getElementById('prioritize').value
+    li.style.color = textColor
+
+    let deleteBtn = document.createElement('button')
+    deleteBtn.textContent = 'Remove'
+
+    let taskList = document.getElementById('tasks')
+   
+    li.appendChild(deleteBtn).addEventListener('click', deleteEvent)
+    taskList.appendChild(li)
   }
+
   function deleteEvent(e) {
     e.target.parentNode.remove()
   }
